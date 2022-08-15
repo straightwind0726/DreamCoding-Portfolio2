162568bb30b2e1e7e18dcecb0e1ab864c2e1159b
straightwind0726 <84476773+straightwind0726@users.noreply.github.com>
2022-08-15 22:57:36 +0900

    js-arrowBtn

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 "use strict";
 
-// transparent navbar
+// transparent navbar + home / display arrowBtn
 
 const navbar = document.querySelector("#navbar");
 const home = document.querySelector("#home");
 const homeHeight = home.getBoundingClientRect().height;
+const arrowBtn = document.querySelector(".arrowBtn");
 
 document.addEventListener("scroll", () => {
     if(window.scrollY > homeHeight / 3) {
         navbar.classList.add("navbar__colored");
+        home.classList.add("home__transparent");
+        arrowBtn.classList.add("arrowBtn__display");
     }
     else {
         navbar.classList.remove("navbar__colored");
+        home.classList.remove("home__transparent");
+        arrowBtn.classList.remove("arrowBtn__display");
     }
 })
 
@@ -20,6 +25,18 @@ document.addEventListener("scroll", () => {
 const navbarMenu = document.querySelector(".navbar__menu");
 
 navbarMenu.addEventListener("click", (event) => {
+    scrollTo(event);
+})
+
+// contact me btn
+
+const homeBtn = document.querySelector(".home__button");
+
+homeBtn.addEventListener("click", (event) => {
+    scrollTo(event);
+})
+
+function scrollTo(event) {
     const target = event.target;
     const link = target.dataset.link;
     if(link == null) {
@@ -27,4 +44,10 @@ navbarMenu.addEventListener("click", (event) => {
     }
     const scrollTo = document.querySelector(link);
     scrollTo.scrollIntoView({behavior: "smooth"});
+}
+
+// move to top when clicking arrow button
+
+arrowBtn.addEventListener("click", (event) => {
+    scrollTo(event);
 })
